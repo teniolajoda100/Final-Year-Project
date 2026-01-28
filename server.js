1323c54d0342301b4fa8827ce661c3075dd280ee
teniolajoda100 <124061707+teniolajoda100@users.noreply.github.com>
2026-01-28 13:20:29 +0000

    Functionality added for cv scannning from prototype

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ const PORT = 3000;
    ================================ */
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'cvision',
-    password: 'REMOVED_SECRET',
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
+
 
 /* ================================
    FILE UPLOAD & OPENAI SETUP
